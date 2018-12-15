682feb6b7920ec493a3d55af7b3d90b7dd041b8a
Erdi Kanik <erdikanik@gmail.com>
2018-12-15 23:13:22 +0300

    Create textfield extension - Add method for searchbar styling

---
+++ FoursquareAPIChallenge/FoursquareAPIChallenge/Extensions/UITextField+FACAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UITextField+FACAdditions.h
+//  FoursquareAPIChallenge
+//
+//  Created by Erdi on 15.12.2018.
+//  Copyright © 2018 erdikanik. All rights reserved.
+//
+
+@interface UITextField (FACAdditions)
+
+- (void)applySearchbarStylingWithPlaceHolderText:(NSString *)placeHolderText;
+
+@end
---
+++ FoursquareAPIChallenge/FoursquareAPIChallenge/Extensions/UITextField+FACAdditions.m
@@ -0,0 +1,33 @@
+//
+//  UITextField+FACAdditions.m
+//  FoursquareAPIChallenge
+//
+//  Created by Erdi on 15.12.2018.
+//  Copyright © 2018 erdikanik. All rights reserved.
+//
+
+#import "UITextField+FACAdditions.h"
+#import "UIFont+FACAdditions.h"
+
+static NSString *kMainSemiboldFont = @"SanFranciscoDisplay-Semibold";
+
+static CGFloat   kSearchBarCornerRadius = 6.0;
+static CGFloat   kSearchBarPlaceHolderFontSize = 17.0;
+
+@implementation UITextField (FACAdditions)
+
+- (void)applySearchbarStylingWithPlaceHolderText:(NSString *)placeHolderText
+{
+    self.layer.cornerRadius = kSearchBarCornerRadius;
+    UIColor *placeHolderColor = [UIColor colorWithRed:199.0 / 255.0 green:199.0 / 255.0 blue:205.0 / 255.0 alpha:1];
+    
+    NSDictionary *attributes = @{
+                                 NSForegroundColorAttributeName : placeHolderColor,
+                                 NSFontAttributeName : [UIFont mainLightFontWithSize:kSearchBarPlaceHolderFontSize]
+                                 };
+    
+    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:placeHolderText
+                                                                 attributes:attributes];
+}
+
+@end
