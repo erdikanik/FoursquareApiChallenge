ff10cdd8a6975ffc9f35aac8e9caa7a7e2c4fd41
Erdi Kanik <erdikanik@gmail.com>
2018-12-15 23:14:31 +0300

    Create main page and controller - Make visual design on storyboard

---
+++ FoursquareAPIChallenge/FoursquareAPIChallenge/Modules/Views/FACMainPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  FACMainPageViewController.m
+//  FoursquareAPIChallenge
+//
+//  Created by Erdi on 15.12.2018.
+//  Copyright © 2018 erdikanik. All rights reserved.
+//
+
+#import "FACMainPageViewController.h"
+
+@interface FACMainPageViewController ()
+
+@end
+
+@implementation FACMainPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)mainButtonTapped:(id)sender {
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
