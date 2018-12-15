faefa099235ac030858eee62920da82e6d366823
Erdi Kanik <erdikanik@gmail.com>
2018-12-15 18:37:57 +0300

    Add time interval for launch screen

--- FoursquareAPIChallenge/FoursquareAPIChallenge/AppDelegate.m
+++ FoursquareAPIChallenge/FoursquareAPIChallenge/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [NSThread sleepForTimeInterval:5.0];
+    
     return YES;
 }
 
