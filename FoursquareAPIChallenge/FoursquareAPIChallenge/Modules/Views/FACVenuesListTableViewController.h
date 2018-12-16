34c53094da89249ee69bfecd9033342a7ef1a8fa
Erdi Kanik <erdikanik@gmail.com>
2018-12-16 13:49:33 +0300

    Add VenuesListTableViewController

---
+++ FoursquareAPIChallenge/FoursquareAPIChallenge/Modules/Views/FACVenuesListTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FACVenuesListTableViewController.h
+//  FoursquareAPIChallenge
+//
+//  Created by Erdi on 16.12.2018.
+//  Copyright © 2018 erdikanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FACVenuesListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
